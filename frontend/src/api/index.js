7a85115525453e1c4d32bd225313cb18f2217d3d
Max_WU <578762865@qq.com>
2025-06-11 13:26:58 +0800

    baseURL: '/api',

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -4,7 +4,7 @@ import router from '../router'
 
 // 创建axios实例
 const api = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: '/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
